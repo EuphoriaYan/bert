4e83ee8e6a8f93d152cb60c5ac229967542a51c6
Euphoria Yan <ysq58000@foxmail.com>
2020-03-17 10:06:11 +0800

    Update scripts.

--- run_pretraining_continue.sh
+++ run_pretraining_continue.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=2
 config_path=configs/bert.json
 input_file='tfrecords/*.*'
 output_dir=model_output
-init_checkpoint=model_output/model.ckpt-600000
+init_checkpoint=model_output/model.ckpt-1000000
 
 python run_pretraining.py \
 --bert_config_file ${config_path} \
@@ -13,7 +13,7 @@ python run_pretraining.py \
 --output_dir ${output_dir} \
 --do_train True \
 --do_eval True \
---num_train_steps 1000000 \
+--num_train_steps 2000000 \
 --num_warmup_steps 0 \
---save_checkpoints_steps 100000 \
+--save_checkpoints_steps 200000 \
 --init_checkpoint ${init_checkpoint}
